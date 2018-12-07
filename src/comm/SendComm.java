a7f2ac3cdfcc0ab9c994f40569b7a04cff917de8
Gnails <liangfuqi2014@qq.com>
2018-12-07 21:21:31 +0800

    reactify
    reactify

--- src/comm/SendComm.java
+++ src/comm/SendComm.java
@@ -3,28 +3,58 @@ import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
 import java.util.*;
-import javax.comm.*;
+//import javax.comm.*;
+import gnu.io.*;
+import gnu.io.SerialPort;
 class SendComm{
 	static Enumeration  portlist;
 	static String comname;
 	static SerialPort sp;
-    static BufferedOutputStream os;
-	public static void main(String args[]) throws PortInUseException, IOException, UnsupportedCommOperationException  {	
+    static OutputStream os;
+	public static void main(String args[]) throws PortInUseException, IOException, UnsupportedCommOperationException, InterruptedException  {	
 	 portlist = CommPortIdentifier.getPortIdentifiers();
 	 while(portlist.hasMoreElements()) {
 		 CommPortIdentifier portid = ( CommPortIdentifier)portlist.nextElement();
 		 System.out.print(portid.getName());
 	    if (portid.getPortType() == CommPortIdentifier.PORT_SERIAL) {
-	    	if(portid.getName().equals(comname)) {
-	    		sp=(SerialPort)portid.open("PortDemo",1000);
-	    		os=(BufferedOutputStream)sp.getOutputStream();
+	    	if(portid.getName().equals("COM5")) {
+	    		System.out.println("open coming");
+	    		sp=(SerialPort)portid.open("SendComm",1000);
+	    		System.out.println("open sp stream");
+	    		os=sp.getOutputStream();
 	    	}
 	    }
 	 }
+	 try {
+		 System.out.println("set param");
 	 sp.setSerialPortParams(9600, SerialPort.DATABITS_8,SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
-	 os.write(0);
+	 for(int j =0; j<5;j++)
+	 for(int i=0; i< 5;i++) {
+     String s = null;
+     switch(i) {
+     case 0:s="0";break;
+     case 1:s="1";break;
+     case 2:s="2";break;
+     case 3:s="3";break;
+     case 4:s="4";break;
+     }
+     byte[] bt=s.getBytes();
+     System.out.printf("Modele%s\n",s);
+	 os.write(bt,0,1);
 	 os.flush();
+	 Thread.sleep(2000);
+}
+	 os.write("0".getBytes(),0,1);
+	 }
+	 catch(Exception e) {
+		 e.printStackTrace();
+		 
+	 }
+	 finally {
+		 if(!os.equals(null))
 	 os.close();
-	 sp.close(); 
+		 if(!sp.equals(null))
+	 sp.close();
+	 }
 }
 }
